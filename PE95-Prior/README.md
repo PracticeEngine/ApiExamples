d909aa8d7815501172aff65519f50b2e8d2d95ba
Ryan Posener <rposener@practice-engine.com>
2018-03-27 10:08:49 -0600

    Add Pre-9.5 Example

---
+++ PE95-Prior/README.md
@@ -0,0 +1,10 @@
+# PE 9.3 through 9.5 and Prior Example
+
+- NOTE: this example is not used for systems running 9.6 and above
+
+Starting with Practice Engine 9.3, a new help section is embedded within the application to help you learn about the WebApi calls which are available.  The WebApi calls are the exact same web calls that our application makes when running in your browser, which means you gain full parity with what is possible in the application and what you can execute. This also means they have more testing, validations and logic built directly into them.
+
+To consume our WebApi there are only 2 things you must do:
+
+1. Create an Application Id and Application Key in the Practice Engine Security Interface
+1. Install the PracticeEngine.WebApi nuget package into your Visual Studio application.
---
+++ PE95-Prior/src/ExampleApiApp/Program.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApiApp
+{
+    /// <summary>
+    /// This is an example API Connection using a Console Application
+    /// Add the 'PracticeEngine.WebApi' nuget package.  This just eases the connection process by using a Handler we have pre-written.
+    /// !!! NUGET is not used for systems running 9.6 and above !!!
+    /// </summary>
+    class Program
+    {
+        // 1. Set/Read your connection details as necessary (typically you want these in a config file somewhere)
+        const string apiBaseAddress = "https://yourserver/PE/";
+        const string appID = "??????????????????";
+        const string appKey = "??????????????????";
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.RunExamples();
+        }
+
+        public Program()
+        {
+
+        }
+
+        public void RunExamples()
+        { 
+            // 2. Create a Handler from our Nuget package, passing in the AppId and App Key
+            var handler = new PE.ApiKey.PEApiKeyHandler(appID, appKey);
+
+
+            // 3. Create standard Http Client with the Handler
+            var peClient = HttpClientFactory.Create(handler);
+
+            // Run Examples
+            Console.WriteLine();
+            Console.WriteLine("Running Synchronous Example");
+            SyncronousExample(peClient);
+
+            Console.WriteLine();
+            Console.WriteLine("Running Asynchronous Example");
+            var task = AsyncronousExample(peClient);
+            task.Wait();
+
+
+            // Just for Run-ability/Readability
+            Console.WriteLine();
+            Console.WriteLine("PRESS ENTER KEY TO EXIT THE DEMO");
+            Console.ReadLine();
+        }
+
+        void SyncronousExample(HttpClient peClient)
+        {
+            // 4. Call the StaffMember/Me function to see who you have logged in as
+            var task = peClient.GetStringAsync(apiBaseAddress + "api/StaffMember/Me");
+
+            // 5. Get the Result
+            var response = task.Result;
+
+            // 6. Convert from JSON
+            JObject obj = JObject.Parse(response);
+
+            // 7. Write out your Name from the PEStaff Object that was Returned
+            Console.WriteLine("Synchronous Returned {0}", obj.Property("StaffName").Value);
+
+        }
+
+        async Task AsyncronousExample(HttpClient peClient)
+        {
+            // 4. Call the StaffMember/Me function to see who you have logged in as
+            var response = await peClient.GetStringAsync(apiBaseAddress + "api/StaffMember/Me");
+
+            // 5. Convert from JSON
+            JObject obj = JObject.Parse(response);
+
+            // 6. Write out your Name from the PEStaff Object that was Returned
+            Console.WriteLine("Asynchronous Returned {0}", obj.Property("StaffName").Value);
+        }
+    }
+}
